64cb035376e4ff73f5fcb40ec3cddba140e0d3e5
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-06-12 15:04:27 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.listen(PORT, (err) => {
 	if (err) {
 		console.log("Error starting server");
+	} else {
+		console.log("Server running on port: " + PORT);
 	}
-	console.log("Server running on port: " + PORT);
 });
-    
