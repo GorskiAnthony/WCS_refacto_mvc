93ecb5b506e17851db01517205a9ce24b7aa3d2f
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-06-12 15:40:55 +0200

    add model

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
-const db = require("./database/config");
+const userModels = require("./models/userModels");
 
 const app = express();
 
 app.get("/", async (req, res) => {
-	const data = await db.query("SELECT * FROM formateur");
-	res.send(data);
+	try {
+		const [users] = await userModels.findAll();
+		res.status(200).send(users);
+	} catch (error) {
+		res.status(500).send("Oups, le serveur est en panne 😅");
+	}
+});
+
+app.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		const data = await userModels.findOne(id);
+		const user = data[0][0];
+		//  [[user]] === user[0][0]
+		res.status(200).send(user);
+	} catch (error) {
+		res.status(500).send("Oups, le serveur est en panne 😅");
+	}
 });
 
 module.exports = app;
---
+++ src/models/userModels.js
@@ -0,0 +1,19 @@
+/**
+ * Nos modèles concernent un formateur (user)
+ */
+
+const db = require("../database/config");
+
+const findAll = () => {
+	const SQL = "SELECT * FROM formateur";
+	return db.query(SQL);
+};
+const findOne = (id) => {
+	const SQL = "SELECT * FROM formateur WHERE id = ?";
+	return db.query(SQL, [id]);
+};
+
+module.exports = {
+	findAll,
+	findOne,
+};
